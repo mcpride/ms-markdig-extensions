c7f0e9bdf1e65287aa853e7c822447639a362548
Marco Stolze <marco.stolze@karlstorz.com>
2023-04-13 12:29:43 +0200

    Small fix to code inline support.

--- src/MS.Markdig.AsciiDoc/Renderers/AsciiDoc/Inlines/CodeInlineRenderer.cs
+++ src/MS.Markdig.AsciiDoc/Renderers/AsciiDoc/Inlines/CodeInlineRenderer.cs
@@ -10,6 +10,13 @@ public class CodeInlineRenderer : AsciiDocObjectRenderer<CodeInline>
 {
     protected override void Write(AsciiDocRenderer renderer, CodeInline obj)
     {
-        renderer.Write($"`{obj.ContentSpan}`");
+        if (obj.ContentSpan.Contains('+'))
+        {
+            renderer.Write($"`pass:[{obj.ContentSpan}]`");
+        }
+        else
+        {
+            renderer.Write($"`+{obj.ContentSpan}+`");
+        }
     }
 }
